57cf4b01ebd7bce72737ca52f0b8dbf59e02ed4d
Gilberto Prudêncio Vaz de Moraes <moraesdev@gmail.com>
2019-11-01 15:48:26 -0300

    add uuid healthplan

--- app/Entities/HealthPlan.php
+++ app/Entities/HealthPlan.php
@@ -4,11 +4,23 @@ namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Entities\Company;
-
+use App\Helpers\Traits\Models\ModelUuidTrait;
 class HealthPlan extends Model
 {
+    use ModelUuidTrait;
+
+    private static $uuidFields = ['label'];
     protected $fillable = ["label"];
     public $timestamps = false;
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            $model["id"] = self::generateUuid($model);
+        });
+    }
 
     public function company()
     {
