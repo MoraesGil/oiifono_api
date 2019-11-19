816503e5550b1d8ed191c1fb986d13f457ea4e95
Gilberto Prudêncio Vaz de Moraes <moraesdev@gmail.com>
2019-11-19 11:54:25 -0300

    typeahead to questionController

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Entities\Question;
+use App\Http\Requests\TypeAheadRequest;
 
 class QuestionController extends Controller
 {
-    //
+    public function typeahead(TypeAheadRequest $request){
+        return Question::where('label', 'like', '%'.$request->get("search_term").'%')->orderBy('label')->limit(15)->get();
+    }
+
 }
