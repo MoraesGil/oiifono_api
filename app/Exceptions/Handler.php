de600d217423b136383a00b15ba3cc163f7ef5b3
gilberto moraes <>
2019-12-13 19:10:05 -0300

    custumized api handler erros

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,13 @@
 
 namespace App\Exceptions;
 
+use Doctrine\DBAL\Query\QueryException;
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Foundation\Testing\HttpException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -13,13 +18,13 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-    \Illuminate\Auth\AuthenticationException::class,
-    \Illuminate\Auth\Access\AuthorizationException::class,
-    \Illuminate\Database\Eloquent\ModelNotFoundException::class,
-    \Illuminate\Session\TokenMismatchException::class,
-    \Illuminate\Validation\ValidationException::class,
+        \Illuminate\Auth\AuthenticationException::class,
+        \Illuminate\Auth\Access\AuthorizationException::class,
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+        \Illuminate\Session\TokenMismatchException::class,
+        \Illuminate\Validation\ValidationException::class,
 
-  ];
+    ];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
@@ -51,6 +56,40 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ValidationException){
+            $errors = $exception->validator->errors()->getMessages();
+
+            $singleErros = Collect(array_keys($errors))->reduce(function ($carry, $item) use ($errors) {
+
+                $carry[$item] = $errors[$item][0];
+
+                return $carry;
+            }, []);
+            return response()->json(["errors" => $singleErros], 422);
+        }
+
+        if ($exception instanceof NotFoundHttpException || $exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'error' => 'Not found'
+            ], 404);
+        } elseif ($exception instanceof HttpException) {
+            return response()->json([
+                'error' => 'Unsupported Media Type'
+            ], 415);
+        } elseif ($exception instanceof AuthenticationException) {
+            return response()->json([
+                'error' => 'Forbidden. Unauthenticated.'
+            ], 403);
+        } elseif ($exception instanceof QueryException) {
+            return response()->json([
+                'error' => 'Unresolvable Query.',
+                'message' => $e->getMessage()
+
+            ], 400);
+        }
+
+
+
         return parent::render($request, $exception);
     }
 }
